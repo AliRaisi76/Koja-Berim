666bea19f1e73a4cfc5ecb00675bf49ea7607b4b
abtinJA42 <abtin.jahanandish99@gmailm.com>
2021-06-01 12:28:41 +0430

    testing a comment

--- models/campground.js
+++ models/campground.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const Review = require('./review')
-
+///ajbnlk
 const campgroundSchema = new Schema({
     title: String,
     image: String,
