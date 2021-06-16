f1117d6ad55dd898de675ddb50d39f44695c1ae2
AliRaisi76 <aliraisi1376@gmail.com>
2021-06-17 02:58:58 +0430

    change password ability added

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ const catchAsync = require('../utils/catchAsync')
 const User = require('../models/user')
 const passport = require('passport')
 const users = require('../controllers/users')
+const { isLoggedIn } = require('../middleware')
+
 
 router.route('/register')
     .get(users.renderRegister)
@@ -15,8 +17,8 @@ router.route('/login')
     .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/users/login' }), users.login)
 
 
-router.get('/:id/edit',users.renderEditUser)
-   
+router.get('/:id/edit', isLoggedIn, users.renderEditUser)
+router.put('/:id', users.updateUser)
 
 
 router.get('/logout', users.logout)
