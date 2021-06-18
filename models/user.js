fe41a2ab05a30dce2a3286de4c0f8bed559ab2dd
abtinJA42 <abtin.jahanandish99@gmailm.com>
2021-06-18 12:20:30 +0430

    final stages of residence

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const Campground = require('./campground')
+const Residence = require('./residence')
 const passportLocalMongoose = require('passport-local-mongoose')
 
 const UserSchema = new Schema({
